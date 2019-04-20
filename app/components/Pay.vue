29ce71564de85e4554653ad8c77ec26a999c63e2
Richard Bondi <r_bondi@hotmail.com>
2019-04-19 21:13:28 -0500

    decode bolt11 for payment

--- app/components/Pay.vue
+++ app/components/Pay.vue
@@ -5,7 +5,8 @@
     </ActionBar>
     <StackLayout>
       <TextView hint="Paste payment request here" v-model="bolt11"/>
-      <TextView hint="Label" v-model="label"/>
+      <TextField hint="Label" v-model="label"/>
+      <TextField hint="Amount(msat)" v-model="amount" :editable="!hasAmount"/>
       <Button dock="top" text="Pay" @tap="payInvoice"/>
     </StackLayout>
   </Page>
@@ -19,20 +20,42 @@ export default {
   data() {
     return {
       bolt11: "",
-      label: ""
+      label: "",
+      amount: "",
+      hasAmount: false
     };
   },
   methods: {
     payInvoice() {
-      this.callRemote("pay", [this.bolt11, null, this.label]).then(data => {
+      this.callRemote("pay", [
+        this.bolt11,
+        this.hasAmount ? null : this.amount,
+        this.label
+      ]).then(data => {
         const result = data.content.toJSON().result;
         if (result.status == "complete") {
           alert(`payment success for ${result.amount_msat}`).then(() => {
-            this.bolt11 = ''
-            this.label = ''
+            this.bolt11 = "";
+            this.label = "";
+            this.amount = "";
           });
         }
       }, console.log);
+    }
+  },
+  watch: {
+    bolt11(b11) {
+      console.log("watching bolt11");
+      if (b11) {
+        this.callRemote("decodepay", [this.bolt11]).then(data => {
+          const result = data.content.toJSON().result;
+          if (result.msatoshi) {
+            this.hasAmount = true;
+            this.amount = result.msatoshi;
+            this.label = result.description;
+          }
+        });
+      }
     }
   }
 };
