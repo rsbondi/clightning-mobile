7be75813c40af773095e700026e3f4b159ef4021
Richard Bondi <r_bondi@hotmail.com>
2019-04-30 16:33:35 -0500

    node screen tweaks

--- app/components/Node.vue
+++ app/components/Node.vue
@@ -5,15 +5,28 @@
     </ActionBar>
     <ScrollView>
       <StackLayout>
-        <FlexboxLayout
+        <!-- <FlexboxLayout
           flexDirection="column-reverse"
           justifyContent="space-around"
           alignItems="stretch"
         >
           <Image :src="qrcode" height="300" width="300" alignSelf="center"/>
-        </FlexboxLayout>
+        </FlexboxLayout> -->
         <WrapLayout>
-          <Label class="label biglabel" text="ID" width="100%"/>
+
+          <Label class="label" text="alias" width="30%"/>
+          <Label :text="info.alias" width="70%"/>
+
+          <Label class="label" text="version" width="30%"/>
+          <Label :text="info.version" width="70%"/>
+
+          <Label class="label" text="network" width="30%"/>
+          <Label :text="info.network" width="70%"/>
+
+          <Label class="label" text="blockheight" width="30%"/>
+          <Label :text="info.blockheight" width="70%"/>
+
+          <Label class="label biglabel" text="id" width="100%"/>
           <TextView :text="info.id"/>
 
           <Label class="label biglabel" text="JSON" width="100%"/>
@@ -30,7 +43,7 @@
 <script>
 import Util from "./util";
 const clipboard = require("nativescript-clipboard");
-const QRCode = require("qrcode");
+// const QRCode = require("qrcode"); // TODO: need some sort of server change to get ip for qr
 
 export default {
   mixins: [Util],
@@ -45,13 +58,13 @@ export default {
       this.callRemote("getinfo").then(data => {
         const result = data.content.toJSON().result;
         this.info = result;
-        QRCode.toDataURL(result.id)
-          .then(id => {
-            this.qrcode = id;
-          })
-          .catch(err => {
-            console.error(err);
-          });
+        // QRCode.toDataURL(result.id)
+        //   .then(id => {
+        //     this.qrcode = id;
+        //   })
+        //   .catch(err => {
+        //     console.error(err);
+        //   });
       });
     }
   }
